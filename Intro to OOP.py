8b7f8616d7a1f5d1c0d5ad8b36c99380597bd507
Rohan Pal <rohanpaluw@gmail.com>
2023-01-29 15:32:14 -0800

    Create Intro to OOP.py

---
+++ Intro to OOP.py
@@ -0,0 +1,53 @@
+class Vehicle(object): #object is a keyword in python
+	def __init__(self, make=None, model=None) #start of object definition block
+		self.make = make
+		self.model = model
+		self.kind = "<unknown>" #initialized with the string of unknown
+		return
+	def getMake(self):
+		return self.make
+	def getModel(self):
+		return self.model
+	def setMake(self, make=""):
+		self.make = make
+		return
+	def setModel(self. model=""):
+		self.model = model
+		return
+
+#GETTER Methods
+connie = Vehicle(make="Conestoga", model="Covered Wagon") #instance of a vehicle
+print(connie.getMake())
+print(connie.getModel())
+print(connie) #gives that it knows that this is an object and where it exists.
+
+prairie = Vehicle(make="Prairie Schooner", model="Covered Wagon")
+print(prairie.getMake(), prairie.getModel())
+print(prairie)
+
+#SETTER Methods
+connie.setModel("Wagon") #Change from Covered Wagon to Wagon
+print(connie.getModel()) #Wagon
+
+
+#To define a landcraft, we have to create a generalized object under Vehicle, which would be a sub-class
+#Landcraft is based on Vehicle instead of an object.
+#So we extend Vehicle with our new class Landcraft
+#Vehicle is the parent or super-class
+
+class Landcraft(Vehicle):
+	def __init__(self, make=None, model=None):
+		Vehicle.__init__(self, make=make, model=model) #we make sure the parent initialization behavior works
+		self.kind = "Landcraft" #This was set to unknown in the original class
+		return
+	def __repr__(self):
+		return f"<Landcraft: make:{self.make} model:{self.model} kind:{self.kind}>"
+
+m1 = Landcraft(make="General Dynamics", model="M1")
+print(m1)
+
+bradley = Landcraft(make="Bae Systems", model="Bradley")
+print(bradley)
+
+m1.setModel("Abrams M1")
+print(m1) #the model would change
